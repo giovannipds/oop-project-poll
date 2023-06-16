51e30c1a76e62284a790810589a25350ac1dde74
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-06-15 21:45:48 -0300

    format

--- src/TelaResultado.java
+++ src/TelaResultado.java
@@ -10,32 +10,32 @@ import java.util.*;
  *
  * @author giovannipds
  */
-public class TelaResultado extends Window{
+public class TelaResultado extends Window {
 
     private Map labels = new HashMap();
 
-    public TelaResultado(Frame parent){
+    public TelaResultado(Frame parent) {
         super(parent);
-        this.setSize(110,120);
-        this.setLayout(new GridLayout(0,2));// Grid com qualquer numero de linhas e uma coluna
+        this.setSize(110, 120);
+        this.setLayout(new GridLayout(0, 2));// Grid com qualquer numero de linhas e uma coluna
         this.add(new Label("Votos"));
         this.add(new Label());
 
     }
 
     /**
-    * Recebe a lista de opcoes inicial
-    */
+     * Recebe a lista de opcoes inicial
+     */
     public void inicializar(Iterator opcoes) {
         String opcao;
         Label label;
         Label votos;
-        while(opcoes.hasNext()){
-            opcao = (String)opcoes.next();
-            if(!labels.containsKey(opcao)){
-                label = new Label(opcao+" - ");
-                votos = new Label(""+0);
-                labels.put(opcao,votos);
+        while (opcoes.hasNext()) {
+            opcao = (String) opcoes.next();
+            if (!labels.containsKey(opcao)) {
+                label = new Label(opcao + " - ");
+                votos = new Label("" + 0);
+                labels.put(opcao, votos);
                 this.add(label);
                 this.add(votos);
             }
@@ -44,8 +44,8 @@ public class TelaResultado extends Window{
 
     public void novoVoto(String opcao, int nvotos) {
         Label votos;
-        votos = (Label)labels.get(opcao);
-        votos.setText(""+nvotos);
+        votos = (Label) labels.get(opcao);
+        votos.setText("" + nvotos);
     }
-    
+
 }
