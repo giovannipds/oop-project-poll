bf2a7e58937a90607d77cb8cbe648cd92bdb9c03
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-06-15 21:30:37 -0300

    class enquete

--- src/Enquete.java
+++ src/Enquete.java
@@ -3,10 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.applet.Applet;
+import java.awt.*;
+
 /**
  *
  * @author giovannipds
  */
-public class Enquete {
+public class Enquete extends Applet{
+    
+    public static void main(String[] args) {
+        Enquete e = new Enquete();
+        e.start();
+    }
+    
+    /**
+    * Inicia o Applet.
+    *
+    * @see java.applet.Applet#start()
+    */
+    public void start() {
+        super.start();
+        // Interface que altera o estado do modelo
+        TelaVotacao votacao = new TelaVotacao();
+        votacao.setLocation(5,5);
+    }
     
 }
